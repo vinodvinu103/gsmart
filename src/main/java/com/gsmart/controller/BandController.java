a9c38870e3bc0722a11ce77a44c90c7115d95adb
Y Vinod kumar Reddy <viratvinu@gmail.com>
2017-06-09 09:08:29 +0000

    BandController.java edited online with Bitbucket

--- src/main/java/com/gsmart/controller/BandController.java
+++ src/main/java/com/gsmart/controller/BandController.java
@@ -205,7 +205,7 @@ public class BandController {
 				respMap.put("status", 400);
 				respMap.put("message", "Data Already Exist, Please try with SomeOther Data");
 			}
-		} else if (task.equals("delete")) {
+		} else {
 			bandServices.deleteBand(band);
 			respMap.put("status", 200);
 			respMap.put("message", "Deleted Successfully");
